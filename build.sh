8e4d31ffc8c6e60f742c3a3f776e8873460e8242
Cody Goodman <cody@itpro.tv>
2020-03-10 14:40:02 -0500

    temporarily remove json and network-bsd

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-nix build '(with import <nixpkgs> (import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz)); haskell-nix.nix-tools)' --out-link nt && nix-env -i ./nt
+nix build '(with import <nixpkgs> (import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz)); haskell-nix.nix-tools)' --out-link nt
+nix-env -i ./nt
+rm -v nt
 stack-to-nix --output . --stack-yaml stack.yaml
+ls -larth *.nix
 nix build -f . haskell-nix-minimal.components.all
