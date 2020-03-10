f2b9fabb61ede6af27285c1168b9747030e2c496
Cody Goodman <cody@itpro.tv>
2020-03-10 14:29:00 -0500

    add build to script

--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 nix build '(with import <nixpkgs> (import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz)); haskell-nix.nix-tools)' --out-link nt && nix-env -i ./nt
 stack-to-nix --output . --stack-yaml stack.yaml
+nix build -f . haskell-nix-minimal.components.all
