c30979b77341faff601c8cc636c39dbb84e85b96
codygman <cody@codygman.dev>
2020-04-06 23:24:21 -0500

    record all attempts version

--- default.nix
+++ default.nix
@@ -8,7 +8,16 @@ let
   pkg = compiler.developPackage
     { root = ./.;
       overrides = self: super: {
-        bson = pkgs.haskellPackages.callHackage "bson" "0.4.0.1" {};
+        bson = pkgs.haskell.lib.disableCabalFlag (
+          pkgs.haskellPackages.callHackage "bson" "0.4.0.1" {}
+        ) "_old-network";
+        # first attempt, this obviously must be it... I'm getting it!
+        network-bsd = pkgs.callHackage "network-bsd" "2.8.1.0" {};
       };
+      source-overrides =
+        {
+          # second attempt, maybe this fixes it
+          network-bsd = pkgs.callHackage "network-bsd" "2.8.1.0" {};
+        };
     };
 in pkg
