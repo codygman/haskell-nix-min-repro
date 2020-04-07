85d76503527d31a33792afc59b5b27cc14a255d1
codygman <cody@codygman.dev>
2020-04-06 23:12:18 -0500

    remove anything not totally necessary

--- default.nix
+++ default.nix
@@ -9,23 +9,6 @@ let
     { root = ./.;
       overrides = self: super: {
         bson = pkgs.haskellPackages.callHackage "bson" "0.4.0.1" {};
-        mongoDB = pkgs.haskellPackages.callHackage "mongoDB" "2.7.0.0" {};
-        HTF = pkgs.haskellPackages.callHackage "HTF" "0.14.0.3" {};
       };
-      source-overrides =
-        {
-          orville = pkgs.fetchFromGitHub {
-            owner = "EdutainmentLive";
-            repo = "orville";
-            rev = "8a2432891e96a547777b67a67135e235a1dce80c";
-            sha256 = "0i8z3zr4z55bzidlh3pz3r3h0hachk5ndfhznw3kqk4c5j6y27ry";
-          };
-          HPDF = pkgs.fetchFromGitHub {
-            owner = "tfausak";
-            repo = "HPDF";
-            rev = "31e2b887cd32661b1ac936335635be5c00216bc8";
-            sha256 = "11fkiqfn6j9qnn114x4pp8rp0frrg2n8dz7gkjr0zndky44mmg32";
-          };
-        };
     };
 in pkg
