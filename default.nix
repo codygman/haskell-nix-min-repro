45fd2fea05ad327876a3b7ace5918bfef429757f
codygman <cody@codygman.dev>
2020-04-09 23:44:44 -0500

    get working; attempt automatic hpack

--- default.nix
+++ default.nix
@@ -1,16 +1,12 @@
-{ compilerVersion ? "ghc883" ,
-  pkgs ?
-  import (builtins.fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/3567e1f6cc204f3b999431ce9e182a86e115976f.tar.gz")
-    {}}:
+{ compiler ? "ghc883" ,
+  pkgs ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/3567e1f6cc204f3b999431ce9e182a86e115976f.tar.gz") {}}:
 let
-  compiler = pkgs.haskell.packages."${compilerVersion}";
-  pkg = compiler.developPackage
+  pinnedHaskell = pkgs.haskell.packages.${compiler};
+  haskell-nix-minimal = pinnedHaskell.developPackage
     { root = ./.;
-      overrides = self: super: {
-        bson = pkgs.haskellPackages.callHackage "bson" "0.4.0.1" {};
-        mongoDB = pkgs.haskellPackages.callHackage "mongoDB" "2.7.0.0" {};
-        HTF = pkgs.haskellPackages.callHackage "HTF" "0.14.0.3" {};
+      overrides = with pkgs.haskell.lib; self: super: {
+        bson = dontCheck (addBuildDepend (markUnbroken super.bson) self.network-bsd);
+        mongoDB = dontCheck (markUnbroken (self.callHackage "mongoDB" "2.7.0.0" {}));
       };
       source-overrides =
         {
@@ -20,12 +16,30 @@ let
             rev = "8a2432891e96a547777b67a67135e235a1dce80c";
             sha256 = "0i8z3zr4z55bzidlh3pz3r3h0hachk5ndfhznw3kqk4c5j6y27ry";
           };
-          HPDF = pkgs.fetchFromGitHub {
-            owner = "tfausak";
-            repo = "HPDF";
-            rev = "31e2b887cd32661b1ac936335635be5c00216bc8";
-            sha256 = "11fkiqfn6j9qnn114x4pp8rp0frrg2n8dz7gkjr0zndky44mmg32";
-          };
         };
+      # This to run when I call nix-shell
+      # My project only has a package.yaml and no cabal file
+      # I need to use cabal v2-repl though so I need to generate it each
+      # time I do:
+      # nix-shell --pure --run "cabal v2-repl"
+      # I think preConfigure hook is correct but this doesn't work
+      modifier = drv:
+        with pkgs.haskellPackages;
+        pkgs.haskell.lib.overrideCabal drv (attrs: {
+          buildTools = [ hlint cabal-install hpack ];
+          # NOTE This doesn't work, I'd like it to generate the cabal file required by cabal-v2 repl
+          # preConfigure = ''
+          # ${pinnedHaskell.hpack}/bin/hpack
+          # '';
+        });
     };
-in pkg
+in haskell-nix-minimal
+
+  # NOTE: this example generates an hpack file on demand like I might need
+  # https://github.com/luc-tielen/typesystem/blob/3b61395b40630c300ffd499efa71166f450579a5/default.nix#L27
+  # snippets from above link:
+  # hpack2cabal = name: src: pkgs.runCommand "hpack2cabal-${name}" {} ''
+  #   ${hpack}/bin/hpack '${src}' - > "$out"
+  # '';
+  # source = nix-gitignore.gitignoreSource [] ./.;
+  # processedSource = hpack2cabal "typesystem" source;
